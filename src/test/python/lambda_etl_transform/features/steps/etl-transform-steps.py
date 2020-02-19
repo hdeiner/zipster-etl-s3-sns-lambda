0342abbe570603d28d44a033339a966234347ccd
howarddeiner <howard.deiner@deinersoft.com>
2020-02-19 12:36:11 -0500

    Added behave based BDD acceptance tests for etl-transform.py

---
+++ src/test/python/lambda_etl_transform/features/steps/etl-transform-steps.py
@@ -0,0 +1,46 @@
+import sys, os
+from behave import *
+from hamcrest import*
+
+@given(u'an input file called "{inputfilename}" with the following contents')
+def step_inputfile_name(context, inputfilename):
+    handle_for_writing_input_file = open(inputfilename, 'w')
+    handle_for_writing_input_file.writelines(context.text)
+    handle_for_writing_input_file.close()
+    pass
+
+@given(u'an error file called "{errorfilename}"')
+def step_errorfile_name(context, errorfilename):
+    context.error_filename = errorfilename
+    pass
+
+@given(u'a load file called "{outputfilename}"')
+def step_output_file_name(context, outputfilename):
+    context.output_filename = outputfilename
+    pass
+
+@when(u'I transform the input file')
+def step_transform_input_file(context):
+    os.system('python3 ../../../main/python/lambda_etl_transform/etl-transform.py -i test.csv -o test.sql -e test.err')
+    pass
+
+@then(u'the error file should contain')
+def step_check_error_file(context):
+    handle_for_reading_errror_file_file = open(context.error_filename, 'r')
+    error_file_lines = handle_for_reading_errror_file_file.readlines()
+    lines_in_error_file = len(error_file_lines)
+    handle_for_reading_errror_file_file.close()
+    lines_expected = context.text.split('\n')
+    for i in range(0, lines_in_error_file-1):
+        assert_that(error_file_lines[i].strip(), equal_to(lines_expected[i].strip()), 'in line '+str(i+1))
+
+@then(u'the load file should contain')
+def step_check_output_file(context):
+    handle_for_reading_output_file_file = open(context.output_filename, 'r')
+    output_file_lines = handle_for_reading_output_file_file.readlines()
+    lines_in_error_file = len(output_file_lines)
+    handle_for_reading_output_file_file.close()
+    lines_expected = context.text.split('\n')
+    for i in range(0, lines_in_error_file-1):
+        assert_that(output_file_lines[i].strip(), equal_to(lines_expected[i].strip()), 'in line '+str(i+1))
+
--- step_1_local_test_etl.sh
+++ step_1_local_test_etl.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+figlet -w 160 -f small "Local Test ETL"
+
+figlet -w 160 -f small "Unit Tests"
 cd src/main/python
-
 pytest lambda_etl_transform/* --verbose
+cd -
 
-python3 lambda_etl_transform/etl-transform.py -i test.csv -o test.sql -e test.err
-
+figlet -w 160 -f small "Acceptance Tests"
+cd src/test/python/lambda_etl_transform
+behave -v
+rm test.csv test.err test.sql
 cd -
